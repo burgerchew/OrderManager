78751764cf3efc4aed462a0a867e9b33e7a0fd0f
Daniel Lindsay <admin@cloudcommerce.net.au>
2025-06-04 16:46:16 +1000

    git add --all

--- Forms/PrintedForm.cs
+++ Forms/PrintedForm.cs
@@ -188,11 +188,6 @@ namespace OrderManagerEF
                 lookupTable.Rows.Add(newRow);
             }
 
-            //repositoryItemLookUpEdit1.DataSource = lookupTable;
-            //repositoryItemLookUpEdit1.ValueMember = "ID";
-            //repositoryItemLookUpEdit1.DisplayMember = "Value";
-
-            //gridView.Columns["ZEmployeeGroup"].ColumnEdit = repositoryItemLookUpEdit1;
         }
 
         private void GridView_CellValueChanged(object sender, CellValueChangedEventArgs e)
